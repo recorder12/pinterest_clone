385fc286ddf23d56785b24a982fadd00aaf17e35
Byeonggeon Kim <byeonggeonmty@gmail.com>
2022-05-01 18:01:58 -0400

    edit display contents part: redirect to accountapp

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -16,7 +17,7 @@ def hello_world(request):
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else :
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
