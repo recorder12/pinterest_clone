5b67805207d3285026aba40d85157f3052d97e50
Byeonggeon Kim <byeonggeonmty@gmail.com>
2022-05-02 00:38:42 -0400

    add account delete part

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'), # CBV need class.as_view() syntax
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'), # pk : primary key (id)
-    path('update/<int:pk>', AccountUpdateView.as_view(), name='update') # pk : primary key (id)
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),# pk : primary key (id)
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete')  # pk : primary key (id)
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -48,3 +48,8 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm  # customized form
     success_url = reverse_lazy('accountapp:hello_world')  # reverse_lazy ~= reverse
     template_name = 'accountapp/update.html'  # view template
+
+class AccountDeleteView(DeleteView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
