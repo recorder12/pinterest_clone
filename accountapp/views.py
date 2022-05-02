52a10f582816ce629e1ce4aece73936c673a7f83
Byeonggeon Kim <byeonggeonmty@gmail.com>
2022-05-02 00:04:39 -0400

    add user profile part

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 from django.urls import reverse, reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accountapp.models import HelloWorld
 
@@ -37,3 +37,7 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world') # reverse_lazy ~= reverse
     template_name = 'accountapp/create.html' # view template
 
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
