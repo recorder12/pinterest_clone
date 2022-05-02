f4b17f18c52b154014652ccabc3e7a75ee49e7d3
Byeonggeon Kim <byeonggeonmty@gmail.com>
2022-05-01 23:30:15 -0400

    add login/logout part

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -138,6 +140,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world') # set redirect url
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login') # set redirect url
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
